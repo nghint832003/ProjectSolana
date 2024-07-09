43895bd01dd71126e4d4adb487955f1b7b5588bf
TheBui2308 <thebui2308@gmail.com>
2024-07-09 13:12:36 +0700

    update: Exams

--- my-shyft-app/src/App.js
+++ my-shyft-app/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { ShyftSdk, Network } from "@shyft-to/js";
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListAll from "./ListAll";
 import CreateNFT from "./CreateNFT";
 import DetailExam from "./views/DetailExam";
+import Exams from "./views/Exams";
 
 function App() {
     return (
@@ -13,7 +13,8 @@ function App() {
                 <Routes>
                     <Route exact path="/" element={<ListAll />} />
                     <Route exact path="/create" element={<CreateNFT />} />
-                    <Route exact path="/exam" element={<DetailExam />} />
+                    <Route exact path="/exam/detail" element={<DetailExam />} />
+                    <Route exact path="/exam" element={<Exams />} />
                 </Routes>
             </Router>
         </div>
