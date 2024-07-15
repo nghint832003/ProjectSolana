038f533bfbe661fe2771d9a44dc017401f19d980
nghint832003 <117883302+nghint832003@users.noreply.github.com>
2024-07-16 01:12:34 +0700

    .

--- my-shyft-app/src/App.js
+++ my-shyft-app/src/App.js
@@ -1,28 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { ShyftSdk, Network } from "@shyft-to/js";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { ShyftSdk, Network } from '@shyft-to/js';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListAll from "./ListAll";
 import CreateNFT from "./CreateNFT";
+import DetailExam from "./views/DetailExam";
 
 import TestList from './components/TestList';
 import CreateTestPage from './components/CreateTestPage';
 
-
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<ListAll />} />
-          <Route exact path="/create" element={<CreateNFT />} />
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<ListAll />} />
 
-          <Route exact path="/showTest" element={<TestList  />} />
-          <Route path="/createTest" element={<CreateTestPage />} /> 
-        </Routes>
-      </Router>
-    </div>
-  );
+                    <Route exact path="/showTest" element={<TestList  />} />
+                    <Route path="/createTest" element={<CreateTestPage />} /> 
+                    
+                    <Route exact path="/create" element={<CreateNFT />} />
+                    <Route exact path="/exam" element={<DetailExam />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
