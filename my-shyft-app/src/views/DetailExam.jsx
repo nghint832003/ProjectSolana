621d7d1a02267b9ab0295094da4277ffc1a7233e
TheBui2308 <thebui2308@gmail.com>
2024-07-16 22:34:10 +0700

    confilict: DetailExam

--- my-shyft-app/src/views/DetailExam.jsx
+++ my-shyft-app/src/views/DetailExam.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Button from "@mui/material/Button";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
