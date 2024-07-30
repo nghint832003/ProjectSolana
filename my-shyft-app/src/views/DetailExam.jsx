1e8e9af2e8d8eb46a3a237d68f5fabb816152949
nghint832003 <117883302+nghint832003@users.noreply.github.com>
2024-07-31 00:02:09 +0700

    fix

--- my-shyft-app/src/views/DetailExam.jsx
+++ my-shyft-app/src/views/DetailExam.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
-import { useParams, useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from "axios";
 import Button from "@mui/material/Button";
 import Radio from "@mui/material/Radio";
@@ -8,6 +8,7 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormLabel from "@mui/material/FormLabel";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CancelIcon from '@mui/icons-material/Cancel';
+import Alert from '@mui/material/Alert';
 
 export default function DetailExam() {
     const { id } = useParams();
@@ -20,11 +21,11 @@ export default function DetailExam() {
     const [timeTaken, setTimeTaken] = useState(0); // time taken in seconds
     const [tabSwitches, setTabSwitches] = useState(0); // Number of tab switches
     const [showTransferButton, setShowTransferButton] = useState(false); // New state for showing transfer button
+    const [message, setMessage] = useState(""); // State for showing messages
     const questionRefs = useRef({});
     const timerIdRef = useRef(null);
-    const navigate = useNavigate(); // Initialize useNavigate
+    const navigate = useNavigate();
 
-    // Fetch questions on component mount
     useEffect(() => {
         const fetchQuestions = async () => {
             try {
@@ -50,7 +51,6 @@ export default function DetailExam() {
         };
     }, [id]);
 
-    // Timer logic
     useEffect(() => {
         if (timeLeft <= 0) {
             handleSubmit();
@@ -64,20 +64,23 @@ export default function DetailExam() {
         return () => clearInterval(timerIdRef.current);
     }, [timeLeft]);
 
-    // Handle answer change
     const handleAnswerChange = useCallback((questionId, answer) => {
         setUserAnswers(prevAnswers => ({ ...prevAnswers, [questionId]: answer }));
     }, []);
 
-    // Handle form submission
     const handleSubmit = useCallback(async () => {
         clearInterval(timerIdRef.current); // Stop the timer
         setTimeTaken(3 * 60 - timeLeft); // Calculate time taken
 
+        console.log("User Answers:", userAnswers); // Check the user answers
+
         const newResults = {};
         let count = 0;
         questions.forEach(question => {
-            const isCorrect = userAnswers[question.id] === question.CorrectOption;
+            const userAnswer = userAnswers[question.id];
+            const correctAnswer = question.CorrectOption;
+            const isCorrect = userAnswer == correctAnswer;
+            console.log(`Question ID: ${question.id}, User Answer: ${userAnswer}, Correct Answer: ${correctAnswer}, Is Correct: ${isCorrect}`);
             newResults[question.id] = isCorrect;
             if (isCorrect) {
                 count++;
@@ -93,7 +96,7 @@ export default function DetailExam() {
         const data = {
             TotalQuestions: questions.length,
             CorrectAnswers: count,
-            Score: (count / questions.length) * 100,
+            Score: (count / questions.length) * 10,
             time_work: 3 * 60 - timeLeft,
             next_page: tabSwitches,
             public_key: publicKey, // Use public_key here
@@ -103,13 +106,17 @@ export default function DetailExam() {
             const response = await axios.post('http://127.0.0.1:8000/api/submit-result', data);
             console.log('Result saved successfully', response.data);
 
-            // Create unique asset
-            const assetResponse = await createUniqueAsset();
-            if (assetResponse && assetResponse.id) {
-                // Set the asset ID in session storage
-                sessionStorage.setItem('unique_asset_id', assetResponse.id);
-                // Show the transfer button immediately after submission
-                setShowTransferButton(true);
+            // Check conditions for creating unique asset
+            if (count > 2 && tabSwitches <= 2) {
+                const assetResponse = await createUniqueAsset();
+                if (assetResponse && assetResponse.id) {
+                    // Set the asset ID in session storage
+                    sessionStorage.setItem('unique_asset_id', assetResponse.id);
+                    setShowTransferButton(true);
+                    setMessage("Bạn đã đạt yêu cầu. Nhấn 'Nhận chứng chỉ' để nhận.");
+                }
+            } else {
+                setMessage("Bạn phải trả lời đúng trên 20 câu và số lần chuyển tab không quá 2 lần mới được nhận chứng chỉ.");
             }
         } catch (error) {
             console.error('There was an error saving the result or creating the unique asset!', error);
@@ -122,48 +129,45 @@ export default function DetailExam() {
             url: 'https://api.gameshift.dev/nx/unique-assets',
             headers: {
                 accept: 'application/json',
-                'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJmYzAxNjMzZS0yNzFhLTQ2ZGEtOGUyZC1lYjVjNTAxODcyNzMiLCJzdWIiOiIxMWE4ODUxZC03ZWViLTQyNjktYTMzOS05MGZiNzAyZjFjYzMiLCJpYXQiOjE3MjE2MzM5MDB9.LRGC8FSwaSogOSbZ50Fnjw_v1Y7T_BcSJCVuG08Inqc', // Replace with your actual API key
+                'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJmYzAxNjMzZS0yNzFhLTQ2ZGEtOGUyZC1lYjVjNTAxODcyNzMiLCJzdWIiOiIxMWE4ODUxZC03ZWViLTQyNjktYTMzOS05MGZiNzAyZjFjYzMiLCJpYXQiOjE3MjE2MzM5MDB9.LRGC8FSwaSogOSbZ50Fnjw_v1Y7T_BcSJCVuG08Inqc',
                 'content-type': 'application/json'
             },
             data: {
                 details: {
-                    collectionId: '6cfdd0b2-9e0c-43ce-8311-eb0f622f330f', // Fixed value
+                    collectionId: '6cfdd0b2-9e0c-43ce-8311-eb0f622f330f',
                     description: 'abc',
                     imageUrl: 'https://pngimg.com/uploads/certified/certified_PNG14.png',
                     name: 'Certification'
                 },
-                destinationUserReferenceId: '1', // Fixed value
+                destinationUserReferenceId: '1',
             }
         };
 
         try {
             const response = await axios.request(options);
             console.log('Unique Asset created successfully', response.data);
-            return response.data; // Return the response data to get the item ID
+            return response.data;
         } catch (error) {
             console.error('There was an error creating the unique asset!', error);
-            throw error; // Re-throw error to be handled in the caller
+            throw error;
         }
     };
 
-    //Store Approval
     const storeApproval = async (publicKey, consentUrl) => {
         try {
-            const response = await axios.post('http://127.0.0.1:8000/api/approvals', {
+            const response = await axios.post('http://127.0.0.1:8000/api/post-approval', {
                 public_key: publicKey,
                 consent_url: consentUrl,
-                status: 0, // Add status here
-                created_at: new Date().toISOString(), // Current date and time
-                updated_at: new Date().toISOString(), // Current date and time
+                status: 0,
+                created_at: new Date().toISOString(),
+                updated_at: new Date().toISOString(),
             });
             console.log('Approval stored successfully', response.data);
         } catch (error) {
             console.error('There was an error storing the approval!', error);
         }
     };
-    
 
-    // Transfer item function and store Approval
     const transferItem = async (itemId) => {
         const publicKey = sessionStorage.getItem('public_key');
         const options = {
@@ -171,59 +175,58 @@ export default function DetailExam() {
             url: `https://api.gameshift.dev/nx/users/1/items/${itemId}/transfer`,
             headers: {
                 accept: 'application/json',
-                'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJmYzAxNjMzZS0yNzFhLTQ2ZGEtOGUyZC1lYjVjNTAxODcyNzMiLCJzdWIiOiIxMWE4ODUxZC03ZWViLTQyNjktYTMzOS05MGZiNzAyZjFjYzMiLCJpYXQiOjE3MjE2MzM5MDB9.LRGC8FSwaSogOSbZ50Fnjw_v1Y7T_BcSJCVuG08Inqc', // Replace with your actual API key
+                'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJmYzAxNjMzZS0yNzFhLTQ2ZGEtOGUyZC1lYjVjNTAxODcyNzMiLCJzdWIiOiIxMWE4ODUxZC03ZWViLTQyNjktYTMzOS05MGZiNzAyZjFjYzMiLCJpYXQiOjE3MjE2MzM5MDB9.LRGC8FSwaSogOSbZ50Fnjw_v1Y7T_BcSJCVuG08Inqc',
                 'content-type': 'application/json',
             },
             data: {
-                destinationWallet: publicKey, // Using publicKey from session storage
-                quantity: '1', // Fixed quantity
+                destinationWallet: publicKey,
+                quantity: '1',
             },
         };
-    
+
         try {
             const response = await axios.request(options);
             console.log('Item transferred successfully', response.data);
-            
-            // Save consentUrl and publicKey to API Approval
+
             const consentUrl = response.data.consentUrl;
             await storeApproval(publicKey, consentUrl);
-            
-            // Optionally handle success response, e.g., show a notification
         } catch (error) {
             console.error('There was an error transferring the item!', error);
-            // Optionally handle error response, e.g., show an error notification
         }
     };
-    
 
-    // Format time helper
     const formatTime = (seconds) => {
         const minutes = Math.floor(seconds / 60);
         const secs = seconds % 60;
         return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
     };
 
-    // Scroll to specific question
     const scrollToQuestion = (questionId) => {
         questionRefs.current[questionId]?.scrollIntoView({ behavior: 'smooth' });
     };
 
     return (
         <div className="bg-[#efefef] p-[24px]">
-            <div className="title flex justify-center items-center gap-x-5">
-                <h2>IELTS Simulation Listening Test 6</h2>
-                <Button variant="outlined">Thoát</Button>
-            </div>
+            
+            {submitted && (
+                <>
+                    <div className="exam-container flex items-start gap-x-[24px] mt-[24px]">
+                        <div className="exam-content bg-white w-[30%] rounded-[8px] p-[16px]">
+                        <Alert severity="info"><p className="text-600">Đáp án đúng: {correctCount}/{questions.length}</p>
+                            <p className="text-600">{`Thời gian làm bài: ${formatTime(timeTaken)}`}</p>
+                            <p className="text-600">Số lần chuyển tab: {tabSwitches}</p></Alert>
+                            
+                        </div>
+                        
+                    </div>
+                </>
+            )}
+
+
             <div className="exam-container flex items-start gap-x-[24px] mt-[24px]">
                 <div className="exam-content bg-white w-[80%] rounded-[8px] p-[16px]">
                     <h3>
-                        {submitted && (
-                            <>
-                                <p>{`Đúng: ${correctCount}/${questions.length}`}</p>
-                                <p>{`Thời gian làm bài: ${formatTime(timeTaken)}`}</p>
-                                <p>{`Số lần chuyển tab: ${tabSwitches}`}</p>
-                            </>
-                        )}
+
                     </h3>
                     <p className="note italic">
                         Choose the correct letter A, B, C, or D
@@ -241,7 +244,7 @@ export default function DetailExam() {
                                     </div>
                                     <div className="question-info ml-[46px] mb-1">
                                         <FormLabel
-                                            id={`radio-buttons-group-label-${question.id}`} // Unique id for each FormLabel
+                                            id={`radio-buttons-group-label-${question.id}`}
                                         >
                                             {question.QuestionText}
                                             <RadioGroup
@@ -275,15 +278,19 @@ export default function DetailExam() {
                                                     disabled={submitted}
                                                 />
                                             </RadioGroup>
+
                                         </FormLabel>
                                         {submitted && (
-                                            <div className="text-red-600 flex items-center">
-                                                {userAnswers[question.id] === question.CorrectOption ? (
-                                                    <CheckCircleIcon />
+                                            <div className="mt-2">
+                                                {results[question.id] ? (
+                                                    <div className="text-green-600 flex items-center">
+                                                        <CheckCircleIcon /> Đúng
+                                                    </div>
                                                 ) : (
-                                                    <CancelIcon />
+                                                    <div className="text-red-600 flex items-center">
+                                                        <CancelIcon /> Sai. Đáp án đúng: {question.CorrectOption}
+                                                    </div>
                                                 )}
-                                                Sai. Đáp án đúng: {question.CorrectOption}
                                             </div>
                                         )}
                                     </div>
@@ -296,9 +303,7 @@ export default function DetailExam() {
                 </div>
                 <div className="exam-overview bg-white w-[20%] rounded p-[16px]">
                     <p>Thời gian còn lại:</p>
-                    <h3 className="text-red-500 text-[24px] font-bold">
-                        {formatTime(timeLeft)}
-                    </h3>
+                    <h3 className="text-red-500 text-[24px] font-bold">{formatTime(timeLeft)}</h3>
                     {!submitted && (
                         <Button
                             variant="contained"
@@ -308,29 +313,30 @@ export default function DetailExam() {
                             NỘP BÀI
                         </Button>
                     )}
-                    {showTransferButton && ( // Conditionally render the transfer button
+                    {showTransferButton && (
                         <Button
-                        variant="contained"
-                        className="w-[100%] !my-3"
-                        onClick={async () => {
-                            const uniqueAssetId = sessionStorage.getItem('unique_asset_id');
-                            if (uniqueAssetId) {
-                                setShowTransferButton(false); // Hide button immediately after click
-                                await new Promise(resolve => setTimeout(resolve, 20000)); // 20 seconds delay
-                                await transferItem(uniqueAssetId);
-                            }
-                        }}
-                    >
-                        Nhận chứng chỉ
-                    </Button>
+                            variant="contained"
+                            className="w-[100%] !my-3"
+                            onClick={async () => {
+                                const uniqueAssetId = sessionStorage.getItem('unique_asset_id');
+                                if (uniqueAssetId) {
+                                    alert('Chứng chỉ sẽ được gửi đến ví của bạn sau ít phút nữa')
+                                    setShowTransferButton(false);
+                                    await new Promise(resolve => setTimeout(resolve, 30000));
+                                    await transferItem(uniqueAssetId);
+                                    
+                                }
+                                
+                            }}
+                            
+                        >
+                            Nhận chứng chỉ
+                        </Button>
                     )}
-                    <p className="text-[14px] text-red-700">
-                        Khôi phục/Lưu bài làm
-                    </p>
-                    <p className="text-orange-500 italic font-bold font-[12px] mt-1">
-                        Chú ý: bạn có thể click vào số thứ tự câu hỏi trong bài
-                        để đánh dấu review
-                    </p>
+
+                    {message && <p className="text-yellow-500 italic font-bold font-[12px] mt-1" >{message}</p>}
+
+                    
                     <div className="status-questions flex items-center gap-1 flex-wrap">
                         {questions.length > 0 && questions.map((ques, index) => (
                             <div
@@ -341,7 +347,12 @@ export default function DetailExam() {
                                 {index + 1}
                             </div>
                         ))}
+                        <p className="text-[14px] text-red-700">
+                        Chú ý: bạn có thể click vào số thứ tự câu hỏi trong bài
+                        để đánh dấu review
+                    </p>
                     </div>
+                    
                 </div>
             </div>
         </div>
